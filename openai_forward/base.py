e0a8390ea6e0512c392573d55b3dad573ef4b721
yuyingtao <yuyingtao@agiclass.ai>
2023-07-11 01:43:35 +0800

    feat: http exception

--- openai_forward/base.py
+++ openai_forward/base.py
@@ -101,7 +101,7 @@ class OpenaiBase:
 
         model = chat_info.get('model')
         if 'gpt-4' in model:
-            raise HTTPException(status=status.HTTP_400_BAD_REQUEST, detail="don't support gpt-4 model")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="don't support gpt-4 model")
 
         req = client.build_request(
             request.method,
