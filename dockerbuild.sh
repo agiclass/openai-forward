4ff581bce95742a0ec15c8f8c15d0f713319eb80
yuyingtao <yuyingtao@agiclass.ai>
2023-07-11 01:08:14 +0800

    feat: change docker image name

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,8 +2,8 @@
  # @Author: yuyingtao yuyingtao@agiclass.ai
  # @Date: 2023-07-10 23:28:31
  # @LastEditors: yuyingtao yuyingtao@agiclass.ai
- # @LastEditTime: 2023-07-10 23:29:58
+ # @LastEditTime: 2023-07-11 01:07:52
  # @Description: docker build
 ### 
 githash=`git rev-parse --short HEAD`
-docker build . -t agi-jupyter:$githash
+docker build . -t agi-openai-forward:$githash
